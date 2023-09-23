b9dff071bcd0cf9d0ce2cb53adc0ce04c7e020db
hongseonet <hongseonmail@gmail.com>
2023-09-23 14:39:28 +0900

    # 바늘 움직인 작업 준비 # textmesh pro 추가 # 프리팹 수정

--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -7,8 +7,13 @@ public class Main : MonoBehaviour
     [SerializeField]
     GameObject hour, min, sec;
 
-    int secIndex, minIndex;
-
+    int idxHour, idxMin, idxSec;
+    
+    /*
+        The hour hand moves 360 degrees / 12 in an hour = 30 degrees.
+        It follows that in a minute it moves 30 degrees / 60 = 1/2 degree.
+        Therefore in a second it moves 1/2 degrees / 60 = 1/120 degree. 
+    */
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +32,15 @@ public class Main : MonoBehaviour
 
     void MoveNIddle(bool isAuto, GameObject target)
     {
+        
+        if (isAuto)
+        {
 
+        }
+        else
+        {
+
+        }
     }
 
     IEnumerator MoveClock()
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Gdv_Edu_Clock
+# 유아 시계 학습 앱
+
+## Summary
+ㄴ 안드로이드 기반의 유아 시계 학습 앱
+ㄴ 시계를 돌리면 빙글빙글 돌아가며, 현재 시간을 숫자로 알려주는 학습
+ㄴ 개발 중...
+
+## Developers
+
+ㄴ Unity 2023.1.9f
+ㄴ VIsual studio 2022
+
+## Community
+
+<a href="https://github.com/bitholla/hollaex-kit/graphs/contributors">
+  <img src="https://contributors-img.web.app/image?repo=bitholla/hollaex-kit" />
+</a>
